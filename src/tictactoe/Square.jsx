6cf3f72a82e48a1b132b304d8c23f9dc96f4b8c9
shreenivas-nayakawadi <shreenivasofficial@gmail.com>
2024-08-16 19:45:13 +0530

    first commit

---
+++ src/tictactoe/Square.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Square = (props) => {
+  return (
+    <div className="square" onClick={props.onClick}>
+      {props.value}
+    </div>
+  );
+};
+export default Square;
